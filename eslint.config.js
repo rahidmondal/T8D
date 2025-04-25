2a7104973f5d8b3dc7fdc5e65e76f15daa595bee
Rahid Mondal <71984696+rahidmondal@users.noreply.github.com>
2025-04-25 17:55:12 +0530

    chore: add prettier and config eslint

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettierConfig from 'eslint-config-prettier';
+
+export default tseslint.config(
+  { ignores: ['**/node_modules/**', '**/dist/**'] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.{js,ts}'],
+    rules: {
+      'no-unused-vars': 'warn',
+    },
+  },
+  prettierConfig
+);
