9aa4349f09fcfc9aa545964b1236a8d599f5f89e
Pushpam Jha <88833138+jhapushpam13@users.noreply.github.com>
2025-05-22 18:52:22 +0530

    feat(pwa): implement PWA support
    - Add service worker and Workbox files for offline support
    - Update index.html and vite.config.ts for PWA integration
    - Add PWA icons (t8d192.png, t8d512.png)
    - Update package.json for PWA metadata
    - Update main.tsx to register service worker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,39 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['t8d192.png', 't8d512.png'],
+      manifest: {
+        name: 'T8D : A To-Do List Application',
+        short_name: 'T8D',
+        description: 'A To-Do List Application',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        start_url: '/',
+        icons: [
+          {
+            src: '/t8d192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/t8d512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true
+      },
+    }),
+  ],
 });
