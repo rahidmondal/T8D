20002f4d0502f4bff351e8ea2ae8ae2ca236093a
Pushpam Jha <88833138+jhapushpam13@users.noreply.github.com>
2025-05-25 13:53:26 +0530

    fix: improve image caching

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -32,6 +32,9 @@ export default defineConfig({
           },
         ],
       },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg,jpeg}'],
+      },
       devOptions: {
         enabled: true,
       },
