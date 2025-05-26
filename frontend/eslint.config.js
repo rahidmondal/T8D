c97aace79d1bdc60486b09f3a3e853d248babf9e
Rahid Mondal <71984696+rahidmondal@users.noreply.github.com>
2025-05-27 03:36:17 +0530

    refactor: enhance ESLint configuration with import plugin and new rules

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,12 +1,13 @@
 import js from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import eslintPluginImport from 'eslint-plugin-import';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist', 'node_modules', 'vite.config.ts', 'vitest.config.ts'] },
+  { ignores: ['dist', 'node_modules', 'vite.config.ts', 'vitest.config.ts', 'dev-dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.strictTypeChecked],
     files: ['**/*.{ts,tsx}'],
@@ -21,6 +22,7 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      import: eslintPluginImport,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -33,6 +35,22 @@ export default tseslint.config(
       'consistent-return': 'warn',
       'default-case': 'warn',
       'react-hooks/exhaustive-deps': 'warn',
+      'import/order': [
+        'warn',
+        {
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type'],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true },
+          pathGroups: [
+            {
+              pattern: 'react',
+              group: 'external',
+              position: 'before',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['react'],
+        },
+      ],
     },
   },
   eslintConfigPrettier,
