0c5568cffc3670d0a495597c120ffd7becef49f2
Pushpam Jha <88833138+jhapushpam13@users.noreply.github.com>
2025-08-06 18:05:48 +0530

    fix: fix the PWA setup and remove unnecessary files

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,8 +6,6 @@ import { createRoot } from 'react-dom/client';
 
 import '@src/index.css';
 
-import { registerSW } from 'virtual:pwa-register';
-
 const rootElement = document.getElementById('root');
 if (rootElement) {
   createRoot(rootElement).render(
@@ -17,13 +15,4 @@ if (rootElement) {
       </ThemeProvider>
     </StrictMode>,
   );
-}
-
-const updateSW = registerSW({
-  onNeedRefresh() {
-    console.log('New content available, please refresh.');
-  },
-  onOfflineReady() {
-    console.log('App is ready to work offline.');
   }
-});
