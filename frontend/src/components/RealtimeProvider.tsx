ace3b30d07e88e46c336bd3a9f72cc724d4afdb9
Rahid Mondal <71984696+rahidmondal@users.noreply.github.com>
2025-11-17 17:51:43 +0530

    fix: websocket being initialized even when sync is disabled

--- frontend/src/components/RealtimeProvider.tsx
+++ frontend/src/components/RealtimeProvider.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 
+import { useSyncState } from '@src/hooks/useSyncState';
+
 import { RealtimeContext } from '../context/RealtimeContext';
 import { useAuth } from '../hooks/useAuth';
 import { disconnectSocket, initializeSocket, T8DSocket } from '../utils/realtime/socket';
@@ -9,11 +11,12 @@ export const RealtimeProvider: React.FC<{ children: React.ReactNode }> = ({ chil
   const { user } = useAuth();
   const [socket, setSocket] = useState<T8DSocket | undefined>(undefined);
   const [isConnected, setIsConnected] = useState(false);
+  const { isSyncEnabled } = useSyncState();
 
   const attachedSocketRef = useRef<T8DSocket | undefined>(undefined);
 
   useEffect(() => {
-    if (user) {
+    if (user && isSyncEnabled) {
       const socketInstance = initializeSocket();
       setSocket(socketInstance);
 
@@ -58,7 +61,7 @@ export const RealtimeProvider: React.FC<{ children: React.ReactNode }> = ({ chil
       setSocket(undefined);
       setIsConnected(false);
     }
-  }, [user]);
+  }, [user, isSyncEnabled]);
 
   useEffect(() => {
     return () => {
